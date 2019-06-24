49b00bc58bcaf7be7be3f2d6b8c66b1be43028ed
Anggoro Gagah Rahardiyawan <arahardiyawan@zamanuniversity.edu.kh>
2019-06-24 14:39:42 +0700

    add functions

--- graphql/resolvers/admin/feedback.js
+++ graphql/resolvers/admin/feedback.js
@@ -1,17 +1,45 @@
+import { Feedbacks } from 'models';
+
 export default {
 
   Query: {
 
     getFeedbackAdmin: async (parent, args, context) => {
       const { limit, skip } = args;
+      const feeds = await Feedbacks.find({})
+        .sort({_id: -1})
+        .skip(skip)
+        .limit(limit)
+        .exec();
+
+      return {
+        ok: true, 
+        feedbacks: feeds
+      };
     },
 
     getPinFeedbackAdmin: async (parent, args, context) => {
       const { limit, skip } = args;
+      const feeds = await Feedbacks.find({ pin: true })
+        .sort({_id: -1})
+        .skip(skip)
+        .limit(limit)
+        .exec();
+
+      return {
+        ok: true, 
+        feedbacks: feeds
+      };
     },
 
     getFeedbackDetailAdmin: async (parent, args, context) => {
       const { feedbackId } = args;
+      const feed = await Feedbacks.findOne({ _id: feedbackId });
+      
+      return {
+        ok: true,
+        feedback: feed
+      };
     }
 
   }
